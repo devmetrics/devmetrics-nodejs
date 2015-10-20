584116a2cb1b8f18fe2ce0c864d3e8d3475a8a69
andrewandrew <andrew@zakonov.com>
2015-10-20 12:54:56 +0300

    support arbitary events structure

--- test/test_devmetrics.js
+++ test/test_devmetrics.js
@@ -1,5 +1,6 @@
 var devmetrics = require('./../lib/devmetrics')({'app_id': 'lib_test'});
-
 devmetrics.userEvent('hey');
 devmetrics.userEvent('hey', ['par_arm1', 'pa98273&*^(%pa2']);
-devmetrics.measure(123, 'hey', ['par_arm1', 'pa98273&*^(%pa2']);
+devmetrics.userEvent('hey', {'hey1': 'par_arm1', 'hey2': 'pa98273&*^(%pa2'});
+devmetrics.measure(12.2, 'hey', ['par_arm1', 'pa98273&*^(%pa2']);
+devmetrics.measure('as', 'hey', ['par_arm1', 'pa98273&*^(%pa2']);
